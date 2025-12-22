f5e892d9bab2028eba55d56960908a926de7a087
kacperovsk <kacperovsk@gmail.com>
2025-12-22 15:57:17 +0100

    Bugfix katapulty, zmienione menu potwierdzenia

--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -1,18 +1,42 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenuController : MonoBehaviour
 {
-    
+    public GameObject optionsPanel;
+    public Toggle autoStartToggle;
+
+    private void Update()
+    {
+        if (Keyboard.current.escapeKey.wasPressedThisFrame)
+        {
+            optionsPanel.SetActive(false);
+        }
+    }
     public void StartGame()
     {
         // Tymczasowe, potem do zastapienia jakims wyborem map.
         SceneManager.LoadScene("LevelSelector");
     }
 
+    public void OpenOptions()
+    {
+        optionsPanel.SetActive(true);
+    }
     public void QuitGame()
     {
         // Zadziala tylko w faktycznej gierce, w unity nie.
         Application.Quit();
+    }
+
+    public void ConfirmOptions()
+    {
+        // do implementacji
+    }
+    public void CancelOptions()
+    {
+        optionsPanel.SetActive(false);
     }
 }
